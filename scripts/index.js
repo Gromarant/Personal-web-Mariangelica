ba0302f0710d350725b44608d602d9c88af7cba5
Mariangelica Rodriguez <gromarant@gmail.com>
2023-04-24 00:30:40 +0200

    Set mobile layout of Home, contacto and portfolio pages

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// "use strict";
+
+// const contactnav = document.getElementById('contact');
+
+
+// const touchHandler = (e) => {
+//   if (e.touches) {
+//     e.preventDefault();
+// };
+// const clickHandler = () => ;
+
+// contactnav.addEventListener('click', );
+// contactnav.addEventListener('touchstart', );
