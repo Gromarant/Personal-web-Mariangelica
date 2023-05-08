b0a37c3144adb2eb373158059000726f6753d3d5
Mariangelica Rodriguez <gromarant@gmail.com>
2023-05-08 08:11:42 +0200

    Add variable of validation

--- index.js
+++ index.js
@@ -17,37 +17,50 @@ const handleForm = (e) => {
   const emailSection = document.querySelector('.email-section');
   const subjectSection = document.querySelector('.subject-section');
   const textAreaSection = document.querySelector('.textArea-section');
+
+  let validated = true
   
   if( name.length < 3) {
+    validated = false;
     if (!helperText){
       setMessage(nameSection, 'El nombre debe tener un mínimo de 3 letras');
     }
   } else if (name.length > 50) {
+    validated = false;
     if (!helperText){
       setMessage(nameSection, 'El nombre es muy largo');
     }
   }
 
   if (!email.endsWith('.com')) {
+    validated = false;
     if (!helperText){
       setMessage(emailSection, 'Solo admite email terminados en .com');
     }
   }
 
   if (subject.length < 3) {
+    validated = false;
     if (!helperText){
       setMessage(subjectSection, 'El mensaje del motivo es muy corto');
     }
   } else if (subject.length > 20) {
+    validated = false;
     if (!helperText){
       setMessage(subjectSection, 'El mensaje del motivo debe ser más largo');
     }
   }
   
   if (message.length > 280) {
+    validated = false;
     if (!helperText){
       setMessage(textAreaSection, 'El mensaje del área de texto debe ser de máximo 280 caracteres');
     }
+  }
+  if (validated) {
+    e.target.submit();
+  } else {
+
   }
 }
 document.querySelector('form').addEventListener("submit", handleForm);
